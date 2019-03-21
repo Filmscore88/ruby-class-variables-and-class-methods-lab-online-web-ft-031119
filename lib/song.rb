cfaf3c41ab8e92ef60d97e9ff325868bdf80497e
Alberto Morales <cmtmorales24@gmail.com>
2019-03-21 20:55:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,8 +19,13 @@ attr_accessor :name, :artist, :genre
   
   def self.genre_count 
     @@genre_count= {}
-    @@genre_count.each do |genre| binding.pry
-      
+    @@genre_count.each do |genre| 
+     
+      if genre_count [genre]
+         genre_count[genre] +=1
+        else 
+          genre_count[genre]=1
+      end 
     end
   end 
   
